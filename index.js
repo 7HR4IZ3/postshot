540fd28f999745332fc3e15914988384bf9051e7
7HR4IZ3 <gamerxville@gmail.com>
2024-11-25 15:20:45 -0800

    feat: update vercel config

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,39 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.send("Express JS")
 });
 
 router.get("/privacy-policy", function (req, res, next) {
-  res.render("privacy-policy", { title: "Privacy Policy" });
+  res.send(`
+    <h1>Privacy Policy</h1>
+    <p>This is a privacy policy.</p>
+    <p>You can find more information about our privacy policy on our website.</p>
+
+    <p>If you have any questions or concerns about our privacy policy, please contact us at info@example.com.</p>
+    <p>Best regards,</p>
+    <p>Example Inc.</p>
+    <p>123 Example Street, Suite 456</p>
+    <p>Example, CA 99999</p>
+    <p>+1 (555) 555-5555</p>
+    <p>www.example.com</p>
+  `);
 });
 
 router.get("/terms-of-service", function (req, res, next) {
-  res.render("terms-of-service", { title: "Terms of Service" });
+  res.send(`
+    <h1>Terms of Service</h1>
+    <p>This is a terms of service.</p>
+    <p>You can find more information about our terms of service on our website.</p>
+
+    <p>If you have any questions or concerns about our terms of service, please contact us at info@example.com.</p>
+    <p>Best regards,</p>
+    <p>Example Inc.</p>
+    <p>123 Example Street, Suite 456</p>
+    <p>Example, CA 99999</p>
+    <p>+1 (555) 555-5555</p>
+    <p>www.example.com</p>
+  `);
 });
 
 export default router;
